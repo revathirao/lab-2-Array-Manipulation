18b3173ee1da35a110161814c59e433b81bfa493
Revathi <revathi9605@gmail.com>
2025-10-27 23:59:45 -0400

    Task 3 complete

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ let shoppingList =[]
 
 // Write a function called addItem that takes an item as a parameter and adds it to the shoppingList array.
 
-function addItem(item){
-  shoppingList.push(item)
-  console.log("Item added to the array " ,item);
-  console.log("Updated shoppingList ", shoppingList);
-}
-addItem("apple")
-addItem("peach")
-addItem("Blueberry")
-addItem("Banana")
-addItem("Chocholate")
-addItem("butter")
-addItem("milk")
+// function addItem(item){
+//   shoppingList.push(item)
+//   console.log("Item added to the array " ,item);
+//   console.log("Updated shoppingList ", shoppingList);
+// }
+// addItem("apple")
+// addItem("peach")
+// addItem("Blueberry")
+// addItem("Banana")
+// addItem("Chocholate")
+// addItem("butter")
+// addItem("milk")
 
 // _______________________________________________________________
 
@@ -35,7 +35,7 @@ addItem("milk")
 //     }
 // }
 // removeLastItem()
-// // ____________________________________________________________________
+// ____________________________________________________________________
 
 // Write a function called displayList that logs all items in the shoppingList array to the console.
 
@@ -58,39 +58,95 @@ addItem("milk")
 // Task 2: Filter and Search an Array
 // Modify the addItem function to only add the item if it is not already in the shoppingList array.
 
-shoppingList =  ['apple', 'peach', 'Blueberry', 'Banana' ]
-function addItem(searchItem) {
-    if (!shoppingList.includes(searchItem)) {
-        shoppingList.push(searchItem)
+// shoppingList =  ['apple', 'peach', 'Blueberry', 'Banana' ]
+// function addItem(searchItem) {
+//     if (!shoppingList.includes(searchItem)) {
+//         shoppingList.push(searchItem)
 
-    } else {
-        console.log("items are already there in the shopping list");
-        // prompt("This item already exists in the cart")
+//     } else {
+//     console.log("items are already there in the shopping list");
 
-    }
-    console.log("Entire list:", shoppingList);
-}
+//     }
+//     console.log("Entire list:", shoppingList);
+// }
 
-addItem("apple")
-addItem("butter")
+// addItem("apple")
+// addItem("butter")
 
 // _______________________________________________//
 
 
 // Write a function called filterItems that takes a search term as a parameter and returns all items
 //  in the shoppingList that contain that search term (case-insensitive).
-shoppingList =['apple', 'peach', 'Blueberry', 'Banana', 'Chocholate', 'butter' ]
 
-function filterItems(searchItem){
+// shoppingList =['apple', 'peach', 'Blueberry', 'Banana', 'Chocholate', 'butter' ]
 
-    let result = shoppingList.filter(item =>item.toLowerCase().includes(searchItem.toLowerCase()))
-    console.log("Filtered Shopping list", result);
-    return result
-}
+// function filterItems(searchItem){
 
-filterItems("b")
-filterItems("apple")
+//     let result = shoppingList.filter(item =>item.toLowerCase().includes(searchItem.toLowerCase()))
+//     console.log("Filtered Shopping list", result);
+//     return result
+// }
+
+// filterItems("b")
+// filterItems("apple")
 
 //________________________________________________________//
 //_________________________________________________________//
 
+// Task 3: Render the List in the Browser
+// Create an HTML page with an input field, an “Add Item” button, and an unordered list to display the items.
+// Write a JavaScript function that adds items to the array and updates the displayed list dynamically when the button is clicked.
+// Write another function that removes the last item and updates the displayed list when a “Remove Last Item” button is clicked.
+
+let cart = []
+let inputItem = document.getElementById("item-input")
+let addItemButton = document.getElementById("addItemButton")
+let removeItemButton = document.getElementById("removeItemButton")
+let cartList = document.getElementById("cart")
+
+// Write a JavaScript function that adds items to the array and updates the displayed list dynamically when the button is clicked.
+
+function addItemToCart() {
+    let item = inputItem.value
+    if (item === "") {
+        alert("Enter the item input")
+        return
+    } else {
+        cart.push(item)
+        displaycartList()
+        inputItem.value ="" //clear item
+    }
+}
+
+// Write a JavaScript function that renoves items to the array and updates the displayed list dynamically when the button is clicked.
+
+function removeItemFromCart() {
+    if (cart.length === 0) {
+        alert("There is no Items to remove")
+        return
+    } else {
+        cart.pop()
+        displaycartList()
+    }
+}
+
+// Write a JavaScript function that displays items in the cart
+
+function displaycartList() {
+    cartList.innerHTML = ""
+
+    if (cart.length === 0) {
+        console.log("Shopping cart is emplty")
+    } else {
+        for (let i = 0; i < cart.length; i++) {
+            let listItem = document.createElement("li")
+            listItem.innerText = cart[i]
+            cartList.appendChild(listItem)
+        }
+    }
+
+}   
+// Attach event listeners
+    addItemButton.addEventListener("click", addItemToCart)
+    removeItemButton.addEventListener("click", removeItemFromCart)
